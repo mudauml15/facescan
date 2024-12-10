cbc6fdc113f8139b5b5968a9be97cc70075c3dc2
Rhendi <mudauml15@hotmail.com>
2024-12-10 15:22:37 +0200

    updated styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Lexend Deca"', "montserrat"], // Set as the default sans-serif font
+      },
+      colors: {
+        "custom-dark": "#242424", // Define your custom color
+      },
+    },
   },
   plugins: [],
-}
+};
