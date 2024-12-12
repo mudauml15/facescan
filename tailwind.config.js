e7b2fff56426f39370236cc557b4392480daff46
Rhendi <mudauml15@hotmail.com>
2024-12-12 09:01:44 +0200

    updated scan buttton

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,24 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
+
       fontFamily: {
         sans: ['"Lexend Deca"', "montserrat"], // Set as the default sans-serif font
       },
+
       colors: {
         "custom-dark": "#242424", // Define your custom color
       },
